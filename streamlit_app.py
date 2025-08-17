f4ea9b7d168c3e2e2637225e4e87d41592384c3c
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-17 08:03:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import numpy as np
 st.title("Data Tools Dashboard")
 st.write("Explore the relationships between and progression of CO₂ Emissions, temperature, GDP per capita, energy use, and number of natural disasters")
 
-if st.button('United States'):
-    st.write('US')
+if st.button('United States's Graph'):
+    st.image('https://raw.githubusercontent.com/samanthaliu525/datatools/05019b5d28930c546e6d3e7f9e5e4cc70a822ec9/images/CO2_temp_US_scaled.png')
 
-if st.button('United States Graph'):
+if st.button('China's Graph'):
     st.image('https://raw.githubusercontent.com/samanthaliu525/datatools/f33a4475b5df43e25839cbf0a420960f32f3bc8e/images/CO2_temp_China_scaled.png')
