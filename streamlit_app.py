05019b5d28930c546e6d3e7f9e5e4cc70a822ec9
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-17 08:01:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ st.write("Explore the relationships between and progression of CO₂ Emissions, 
 if st.button('United States'):
     st.write('US')
 
+if st.button('United States Graph'):
+    st.image('https://raw.githubusercontent.com/samanthaliu525/datatools/f33a4475b5df43e25839cbf0a420960f32f3bc8e/images/CO2_temp_China_scaled.png')
