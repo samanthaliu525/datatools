e3bd57223764b86a23b2317ee2e9a4c0114d076b
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-17 07:56:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Data Tools Dashboard")
+st.write("Explore the relationships between and progression of CO₂ Emissions, temperature, GDP per capita, energy use, and number of natural disasters")
+
+if st.button('United States'):
+    st.write('US')
+
