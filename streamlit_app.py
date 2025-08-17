14edbf5fd5a7a10e6439859214b7b11cffdf4766
samanthaliu525 <samantha.liu525@gmail.com>
2025-08-17 08:24:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,60 +5,28 @@ import numpy as np
 st.title("Data Tools Dashboard")
 st.write("Explore the relationships between and progression of CO₂ Emissions, temperature, GDP per capita, energy use, and number of natural disasters")
 
-st.subheader('Emissions and Temperature')
-CO2_temp_US_facet_url = "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/d3f902e6facd88596ae1367741663ba9119887/CO2_temp_US_facet.png"
+# Define the URLs for your pre-rendered PNG files on GitHub.
+CO2_temp_US_scaled_url = "https://raw.githubusercontent.com/samanthaliu525/datatools/05019b5d28930c546e6d3e7f9e5e4cc70a822ec9/images/CO2_temp_US_scaled.png"
+CO2_temp_US_facet_url = "https://raw.githubusercontent.com/samanthaliu525/datatools/d3f902e6facd88596ae1367741663ba9119887/images/CO2_temp_US_facet.png"
+CO2_temp_China_scaled_url = "https://raw.githubusercontent.com/samanthaliu525/datatools/f33a4475b5df43e25839cbf0a420960f32f3bc8e/images/CO2_temp_China_scaled.png"
 CO2_temp_China_facet_url = "https://raw.githubusercontent.com/samanthaliu525/datatools/8c94c7c43a14d595c92ecaaa56b4f172809653d3/images/CO2_temp_China_facet%20(1).png"
 
-# Create buttons to choose the plot to display
-col1, col2 = st.columns(2)
-with col1:
-    show_us = st.button("United States")
-with col2:
-    show_china = st.button("China")
+# Create a selectbox (dropdown menu) to choose the plot to display
+chart_options = [
+    "US Scaled",
+    "US Facet",
+    "China Scaled",
+    "China Facet"
+]
 
-# Use a state variable to track which button was clicked last
-if 'last_clicked' not in st.session_state:
-    st.session_state.last_clicked = 'us'
+selected_chart = st.selectbox("Select a chart to view:", chart_options)
 
-if show_us:
-    st.session_state.last_clicked = 'us'
-elif show_china:
-    st.session_state.last_clicked = 'china'
-
-# Display the correct image based on the last clicked button
-if st.session_state.last_clicked == 'us':
-    st.image(CO2_temp_US_facet_url, caption="United States")
-elif st.session_state.last_clicked == 'china':
-    st.image(CO2_temp_China_facet_url, caption="China")
-else: # A default plot if no button has been clicked yet.
-    st.image(CO2_temp_US_facet_url, caption="United States")
-
-
-st.subheader('Emissions vs. Temperature - Scaled')
-CO2_temp_US_scaled_url = "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/d3f902e6facd88596ae1367741663ba914119887/CO2_temp_US_scaled.png"
-CO2_temp_China_scaled_url = "https://raw.githubusercontent.com/samanthaliu525/datatoolsdashboard/37b578aaecd79f967b66478f6a56ffe878e217ee/CO2_temp_China_scaled.png"
-
-
-# Create buttons to choose the plot to display
-col1, col2 = st.columns(2)
-with col1:
-    show_us = st.button("United States")
-with col2:
-    show_china = st.button("China")
-
-# Use a state variable to track which button was clicked last
-if 'last_clicked' not in st.session_state:
-    st.session_state.last_clicked = 'us'
-
-if show_us:
-    st.session_state.last_clicked = 'us'
-elif show_china:
-    st.session_state.last_clicked = 'china'
-
-# Display the correct image based on the last clicked button
-if st.session_state.last_clicked == 'us':
-    st.image(CO2_temp_US_scaled_url, caption="United States")
-elif st.session_state.last_clicked == 'china':
-    st.image(CO2_temp_China_scaled_url, caption="China")
-else: # A default plot if no button has been clicked yet.
-    st.image(CO2_temp_US_scaled_url, caption="United States")
+# Display the correct image based on the selected option
+if selected_chart == "US Scaled":
+    st.image(CO2_temp_US_scaled_url, caption="United States - Scaled")
+elif selected_chart == "US Facet":
+    st.image(CO2_temp_US_facet_url, caption="United States - Facet")
+elif selected_chart == "China Scaled":
+    st.image(CO2_temp_China_scaled_url, caption="China - Scaled")
+elif selected_chart == "China Facet":
+    st.image(CO2_temp_China_facet_url, caption="China - Facet")
